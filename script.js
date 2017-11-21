7066db61bfebfa8ae20278959a8937b6b0dabde9
Unknown <hudson_miyajima@hotmail.com>
2017-11-21 12:58:52 -0300

    Inicio de Projeto

---
+++ script.js
@@ -0,0 +1,44 @@
+var PopupController = function () {
+    this.formatacao_checkbox = document.getElementById('formatacao');
+    this.cpf_button = document.getElementById('cpf');
+    this.cnpj_button = document.getElementById('cnpj');
+    this.numero_field = document.getElementById('numero');
+    this.mensagem = document.getElementById('msg');
+    this.addListeners_();
+};
+
+PopupController.prototype = {
+
+    addListeners_: function () {
+        this.cpf_button.addEventListener('click', this.geraNumeroDocumentoCpf.bind(this));
+        this.cnpj_button.addEventListener('click', this.geraNumeroDocumentoCnpj.bind(this));
+    },
+    geraNumeroDocumentoCpf: function () {
+        var formatacao = this.formatacao_checkbox.checked;
+
+        this.numero_field.value = geraCpf(formatacao);
+
+        this.copiaParaAreaDeTransferencia();
+
+    },
+    geraNumeroDocumentoCnpj: function () {
+        var formatacao = this.formatacao_checkbox.checked;
+
+        this.numero_field.value = geraCnpj(formatacao);
+
+        this.copiaParaAreaDeTransferencia();
+
+    },
+    copiaParaAreaDeTransferencia: function () {
+
+        this.numero_field.select();
+        document.execCommand('copy');
+        this.mensagem.textContent = 'Copiado para área de transferência';
+        this.mensagem.setAttribute('class', 'label label-primary');
+    }
+
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+    window.PC = new PopupController();
+});
