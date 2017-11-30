caf99d48820a581f7d7c5dda8f17a7fabed2cfbc
Unknown <hudson_miyajima@hotmail.com>
2017-11-30 15:30:07 -0300

    Auto stash before merge of "master" and "1.1_salva_na_memoria"
    Adicionado a função de salvar na memória

--- js/script.js
+++ js/script.js
@@ -4,28 +4,56 @@ var PopupController = function () {
     this.cnpj_button = document.getElementById('cnpj');
     this.numero_field = document.getElementById('numero');
     this.mensagem = document.getElementById('msg');
+    this.teste = '';
+    this.init();
     this.addListeners_();
 };
 
 PopupController.prototype = {
-
+    init: function () {
+        chrome.storage.sync.get("value", function (item) {
+            if (!chrome.runtime.error) {
+                console.log(item.value);
+                if (item.value !== undefined) {
+                    document.getElementById("numero").value = item.value;
+                }
+            }
+        });
+    },
     addListeners_: function () {
         this.cpf_button.addEventListener('click', this.geraNumeroDocumentoCpf.bind(this));
         this.cnpj_button.addEventListener('click', this.geraNumeroDocumentoCnpj.bind(this));
     },
     geraNumeroDocumentoCpf: function () {
         this.numero_field.value = geraCpf(this.formatacao_checkbox.checked);
+<<<<<<< Updated upstream
         this.copiaParaAreaDeTransferencia();
     },
     geraNumeroDocumentoCnpj: function () {
         this.numero_field.value = geraCnpj(this.formatacao_checkbox.checked);
         this.copiaParaAreaDeTransferencia();
+=======
+        this.copiaParaAreaDeTransferencia(this.numero_field.value);
+        this.copiaParaMemoria(this.numero_field.value);
+    },
+    geraNumeroDocumentoCnpj: function () {
+        this.numero_field.value = geraCnpj(this.formatacao_checkbox.checked);
+        this.copiaParaAreaDeTransferencia(this.numero_field.value);
+        this.copiaParaMemoria(this.numero_field.value);
+>>>>>>> Stashed changes
     },
     copiaParaAreaDeTransferencia: function () {
         this.numero_field.select();
         document.execCommand('copy');
         this.mensagem.textContent = 'Copiado para área de transferência';
         this.mensagem.setAttribute('class', 'label label-primary');
+    },
+    copiaParaMemoria: function (documento) {
+        chrome.storage.sync.set({'value': documento}, function () {
+            if (chrome.runtime.error) {
+                console.log("Runtime error.");
+            }
+        });
     }
 };
 
