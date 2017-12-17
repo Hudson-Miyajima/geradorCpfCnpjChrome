2cb2eb6c8a218b88557fc3b58240bdf9ba63bc14
Unknown <hudson_miyajima@hotmail.com>
2017-12-17 19:29:13 -0300

    Refactoring

--- js/documentoFactory.js
+++ js/documentoFactory.js
@@ -1,62 +1,128 @@
-function randomiza(n) {
-    var ranNum = Math.round(Math.random() * n);
-    return ranNum;
+randomiza = function() {
+  return Math.round(Math.random() * 9);
 };
 
-function mod(dividendo, divisor) {
-    return Math.round(dividendo - (Math.floor(dividendo / divisor) * divisor));
+mod = function(dividendo, divisor) {
+  return Math.round(dividendo - (Math.floor(dividendo / divisor) * divisor));
 };
 
-function geraCpf(comPonto) {
-    var n = 9;
-    var n1 = this.randomiza(n);
-    var n2 = this.randomiza(n);
-    var n3 = this.randomiza(n);
-    var n4 = this.randomiza(n);
-    var n5 = this.randomiza(n);
-    var n6 = this.randomiza(n);
-    var n7 = this.randomiza(n);
-    var n8 = this.randomiza(n);
-    var n9 = this.randomiza(n);
-    var d1 = n9 * 2 + n8 * 3 + n7 * 4 + n6 * 5 + n5 * 6 + n4 * 7 + n3 * 8 + n2 * 9 + n1 * 10;
-    d1 = 11 - ( this.mod(d1, 11) );
-    if (d1 >= 10) d1 = 0;
-    var d2 = d1 * 2 + n9 * 3 + n8 * 4 + n7 * 5 + n6 * 6 + n5 * 7 + n4 * 8 + n3 * 9 + n2 * 10 + n1 * 11;
-    d2 = 11 - ( this.mod(d2, 11) );
-    if (d2 >= 10) d2 = 0;
-    var documento = '';
-    if (comPonto)
-        documento = '' + n1 + n2 + n3 + '.' + n4 + n5 + n6 + '.' + n7 + n8 + n9 + '-' + d1 + d2;
-    else
-        documento = '' + n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + d1 + d2;
-    return documento;
+Numero = function(cpf) {
+  return {
+    n1: randomiza(),
+    n2: randomiza(),
+    n3: randomiza(),
+    n4: randomiza(),
+    n5: randomiza(),
+    n6: randomiza(),
+    n7: randomiza(),
+    n8: randomiza(),
+    n9: cpf === true ? randomiza() : 0,
+    n10: 0,
+    n11: 0,
+    n12: 1,
+  };
+};
+geraCpf = function(comPonto) {
+  var numero_ = new Numero(true);
+
+  var d1 =
+      numero_.n9 * 2 +
+      numero_.n8 * 3 +
+      numero_.n7 * 4 +
+      numero_.n6 * 5 +
+      numero_.n5 * 6 +
+      numero_.n4 * 7 +
+      numero_.n3 * 8 +
+      numero_.n2 * 9 +
+      numero_.n1 * 10;
+
+  d1 = 11 - ( this.mod(d1, 11) );
+
+  if (d1 >= 10) {
+    d1 = 0;
+  }
+  var d2 =
+      d1 * 2 +
+      numero_.n9 * 3 +
+      numero_.n8 * 4 +
+      numero_.n7 * 5 +
+      numero_.n6 * 6 +
+      numero_.n5 * 7 +
+      numero_.n4 * 8 +
+      numero_.n3 * 9 +
+      numero_.n2 * 10 +
+      numero_.n1 * 11;
+
+  d2 = 11 - ( this.mod(d2, 11) );
+
+  if (d2 >= 10) {
+    d2 = 0;
+  }
+
+  return {
+    comPonto: '' + numero_.n1 + numero_.n2 + numero_.n3 + '.' + numero_.n4 +
+    numero_.n5 + numero_.n6 + '.' + numero_.n7 + numero_.n8 + numero_.n9 +
+    '-' + d1 + d2,
+    semPonto: '' + numero_.n1 + numero_.n2 + numero_.n3 + numero_.n4 +
+    numero_.n5 +
+    numero_.n6 + numero_.n7 + numero_.n8 + numero_.n9 + d1 + d2,
+  };
 };
 
-function geraCnpj(comPonto) {
-    var n = 9;
-    var n1 = this.randomiza(n);
-    var n2 = this.randomiza(n);
-    var n3 = this.randomiza(n);
-    var n4 = this.randomiza(n);
-    var n5 = this.randomiza(n);
-    var n6 = this.randomiza(n);
-    var n7 = this.randomiza(n);
-    var n8 = this.randomiza(n);
-    var n9 = 0; //randomiza(n);
-    var n10 = 0; //randomiza(n);
-    var n11 = 0; //randomiza(n);
-    var n12 = 1; //randomiza(n);
-    var d1 = n12 * 2 + n11 * 3 + n10 * 4 + n9 * 5 + n8 * 6 + n7 * 7 + n6 * 8 + n5 * 9 + n4 * 2 + n3 * 3 + n2 * 4 + n1 * 5;
-    d1 = 11 - ( this.mod(d1, 11) );
-    if (d1 >= 10) d1 = 0;
-    var d2 = d1 * 2 + n12 * 3 + n11 * 4 + n10 * 5 + n9 * 6 + n8 * 7 + n7 * 8 + n6 * 9 + n5 * 2 + n4 * 3 + n3 * 4 + n2 * 5 + n1 * 6;
-    d2 = 11 - ( this.mod(d2, 11) );
-    if (d2 >= 10) d2 = 0;
-    var documento = '';
-    if (comPonto)
-        documento = '' + n1 + n2 + '.' + n3 + n4 + n5 + '.' + n6 + n7 + n8 + '/' + n9 + n10 + n11 + n12 + '-' + d1 + d2;
-    else
-        documento = '' + n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + n10 + n11 + n12 + d1 + d2;
-    return documento;
+geraCnpj = function(comPonto) {
+  var numero_ = new Numero(false);
+  console.log(numero_.n9);
+
+  var d1 =
+      numero_.n12 * 2 +
+      numero_.n11 * 3 +
+      numero_.n10 * 4 +
+      numero_.n9 * 5 +
+      numero_.n8 * 6 +
+      numero_.n7 * 7 +
+      numero_.n6 * 8 +
+      numero_.n5 * 9 +
+      numero_.n4 * 2 +
+      numero_.n3 * 3 +
+      numero_.n2 * 4 +
+      numero_.n1 * 5;
+
+  d1 = 11 - ( this.mod(d1, 11) );
+
+  if (d1 >= 10) {
+    d1 = 0;
+  }
+
+  var d2 =
+      d1 * 2 +
+      numero_.n12 * 3 +
+      numero_.n11 * 4 +
+      numero_.n10 * 5 +
+      numero_.n9 * 6 +
+      numero_.n8 * 7 +
+      numero_.n7 * 8 +
+      numero_.n6 * 9 +
+      numero_.n5 * 2 +
+      numero_.n4 * 3 +
+      numero_.n3 * 4 +
+      numero_.n2 * 5 +
+      numero_.n1 * 6;
+
+  d2 = 11 - ( this.mod(d2, 11) );
+
+  if (d2 >= 10) {
+    d2 = 0;
+  }
+  return {
+    comPonto: '' + numero_.n1 + numero_.n2 +
+    '.' + numero_.n3 + numero_.n4 + numero_.n5 +
+    '.' + numero_.n6 + numero_.n7 + numero_.n8 +
+    '/' + numero_.n9 + numero_.n10 + numero_.n11 + numero_.n12 + '-' + d1 +
+    d2,
+    semPonto: '' + numero_.n1 + numero_.n2 + numero_.n3 + numero_.n4 +
+    numero_.n5 + numero_.n6 + numero_.n7 + numero_.n8 + numero_.n9 +
+    numero_.n10 + numero_.n11 +
+    numero_.n12 + d1 + d2,
+  };
 };
 
---
+++ js/index.js
@@ -0,0 +1,56 @@
+PopupController = function() {
+  this.format_check = document.getElementById('format');
+  this.cpf_button = document.getElementById('cpf');
+  this.cnpj_button = document.getElementById('cnpj');
+  this.numero_field = document.getElementById('numero');
+  this.mensagem = document.getElementById('msg');
+  this.init();
+  this.addListeners_();
+};
+
+PopupController.prototype = {
+  init: function() {
+    chrome.storage.sync.get('value', function(item) {
+      if (!chrome.runtime.error) {
+        if (item.value !== undefined) {
+          document.getElementById('numero').value = item.value;
+        }
+      }
+    });
+  },
+  addListeners_: function() {
+    this.cpf_button.addEventListener('click', this.geraNumCpf.bind(this));
+    this.cnpj_button.addEventListener('click', this.geraNumCnpj.bind(this));
+  },
+  geraNumCpf: function() {
+    this.numero_field.value = this.format_check.checked === true ?
+        geraCpf().comPonto :
+        geraCpf().semPonto;
+    this.copiaParaAreaDeTransferencia(this.numero_field.value);
+    this.copiaParaMemoria(this.numero_field.value);
+  },
+  geraNumCnpj: function() {
+    this.numero_field.value = this.format_check.checked === true ?
+        geraCnpj().comPonto :
+        geraCnpj().semPonto;
+    this.copiaParaAreaDeTransferencia(this.numero_field.value);
+    this.copiaParaMemoria(this.numero_field.value);
+  },
+  copiaParaAreaDeTransferencia: function() {
+    this.numero_field.select();
+    document.execCommand('copy');
+    this.mensagem.textContent = 'Copiado para área de transferência';
+    this.mensagem.setAttribute('class', 'label label-primary');
+  },
+  copiaParaMemoria: function(documento) {
+    chrome.storage.sync.set({'value': documento}, function() {
+      if (chrome.runtime.error) {
+        console.log('Runtime error.');
+      }
+    });
+  },
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+  window.PC = new PopupController();
+});
